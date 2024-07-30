32ddce32495c9c0d7b5a2a52cea37ae8683aa71a
iset <iset@localhost-live.home>
2024-07-31 02:05:56 +0300

    mail test8

--- contact-form/server.js
+++ contact-form/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
